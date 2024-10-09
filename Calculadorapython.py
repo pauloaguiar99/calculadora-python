4fe6e67e1fa5605c7e110b1b057a799b2cf4b176
Paulo Aguiar <pvga@cesar.school>
2024-10-09 14:13:35 -0300

    Inserindo arquivo calculadora python

---
+++ Calculadorapython.py
@@ -0,0 +1,7 @@
+num1 = int(input ("Informe um número"))
+num2 = int(input ("Informe outro número"))
+
+print ("A soma entre os dois números é: ", num1 + num2)
+print ("A subtração entre os dois números é: ", num1 - num2)
+print ("A multiplicação entre os dois números é: ", num1 * num2)
+print ("A divisão entre os dois números é: ", num1/num2) 
